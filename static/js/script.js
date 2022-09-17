e652bf0eca45c0a9e73297445bbc800cfbd6be8c
ekosutrisno1 <ekosutrisno801@gmail.com>
2022-09-17 15:03:29 +0700

    init: add all assets

---
+++ static/js/script.js
@@ -0,0 +1,7 @@
+window.Userback = window.Userback || {};
+Userback.access_token = '37034|72540|fzDx3cHeqETmW3uN969UqrO0F';
+(function(d) {
+    var s = d.createElement('script');s.async = true;
+    s.src = 'https://static.userback.io/widget/v1.js';
+    (d.head || d.body).appendChild(s);
+})(document);
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.html", "./themes/briix-theme/layouts/**/*.html",],
+  theme: {
+    extend: {
+      colors: {
+        "briix-primary": "#1F9ED3",
+        "briix-black": "#000000",
+        "briix-white": "#FFFFFF",
+        "briix-body": "#666666",
+        "briix-blue-1": "#EAF6FB",
+        "briix-blue-3": "#99D2EB",
+        "briix-blue-6": "#135F7F",
+        "briix-blue-7": "#0C3F54",
+        "briix-grey-2": "#E3E3E3",
+        "briix-grey-6": "#606060",
+        "briix-grey-7": "#262626",
+        "briix-black-100": "#333333",
+      }
+    },
+  },
+  plugins: [],
+}
