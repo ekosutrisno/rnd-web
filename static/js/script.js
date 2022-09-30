320f6ead5175f0ccf337eaa4afcbb8f9539d20d3
Eko Sutrisno <eko@briix.com>
2022-09-30 11:39:19 +0700

    update: add carrousel library

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,12 @@
-window.Userback = window.Userback || {};
-Userback.access_token = '37034|72540|fzDx3cHeqETmW3uN969UqrO0F';
-(function(d) {
-    var s = d.createElement('script');s.async = true;
-    s.src = 'https://static.userback.io/widget/v1.js';
-    (d.head || d.body).appendChild(s);
-})(document);
+var elem = document.querySelector('.main-carousel');
+var flkty = new Flickity( elem, {
+  // options
+  cellAlign: 'left',
+  contain: true
+});
+
+// element argument can be a selector string
+//   for an individual element
+var flkty = new Flickity( '.main-carousel', {
+  // options
+});
